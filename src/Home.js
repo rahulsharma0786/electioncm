4dbb6d0268e7aeaa314d3a3be9512bea7268a89c
rahulsharma0786 <77057152+rahulsharma0786@users.noreply.github.com>
2024-01-05 15:00:22 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,16 @@
 import { useState } from "react"
 import axios from "axios"
+import surendra from "./surendra.jpg"
+import car from "./car.png"
+import dharmendra from "./dharmendra.jpeg"
+import Jhopdi from "./jhopi1.jpg"
+
+import santosh from "./santosh.jpg"
+import tea from "./tea.jpg"
+
+
+import rajesh from "./rajesh.jpg"
+import peg from "./peg.jpg"
 const Register=()=>{
 
 const[input,setInput]=useState({})
@@ -27,6 +38,47 @@ axios.post("https://election-u79u.onrender.com/stusave",input).then(()=>{
 
     return(
         <>
+        <div>
+            <div>
+                <h2 style={{color:"red", fontWeight:"bolder",marginTop:"10px",marginLeft:"30px"}}>X:-Cm Candidate-:X</h2>
+                
+                <div style={{display:"flex",marginTop:"30px"}}>
+            <img src={surendra} height="120px" width="150px" style={{borderRadius:"10%"}}/>
+            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+                <h3>surendra Dodiya</h3>
+                <h4>Profeshan:Teacher</h4>
+                <img src={car} style={{height:"70px",width:"70px"}}/>
+            </div>
+            </div>
+
+            <div style={{display:"flex",marginTop:"10px"}}>
+            <img src={santosh} height="120px" width="150px" style={{borderRadius:"10%"}}/>
+            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+                <h3>santosh Meena</h3>
+                <h4>Profeshan:Tea bussness</h4>
+                <img src={tea} style={{height:"50px",width:"70px"}}/>
+            </div>
+            </div>
+
+            <div style={{display:"flex",marginTop:"10px"}}>
+            <img src={dharmendra} height="120px" width="150px" style={{borderRadius:"10%"}}/>
+            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+                <h3>Dharmendra sharma</h3>
+                <h4>Profeshan:Lic agent</h4>
+                <img src={Jhopdi} style={{height:"50px",width:"80px"}}/>
+            </div>
+            </div>
+
+            <div style={{display:"flex",marginTop:"10px"}}>
+            <img src={rajesh} height="120px" width="150px" style={{borderRadius:"10%"}}/>
+            <div style={{height:"120px",width:"150px", border:"1px dashed blue" }}>
+                <h3>Rajesh Raniya</h3>
+                <h4>Profeshan:Bussness</h4>
+                <img src={peg} style={{height:"60px",width:"80px"}}/>
+            </div>
+            </div>
+            </div>
+            </div>
 
 <div id="record">
 <h2 style={{color:"red"}}>EVoting For Cm Candidate</h2>    
