e095feed3bcdcc3adff729f8e1a695273a70b22e
rahulsharma0786 <77057152+rahulsharma0786@users.noreply.github.com>
2024-01-05 15:25:14 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -44,16 +44,16 @@ axios.post("https://election-u79u.onrender.com/stusave",input).then(()=>{
                 
                 <div style={{display:"flex",marginTop:"30px"}}>
             <img src={surendra} height="120px" width="150px" style={{borderRadius:"10%"}}/>
-            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+            <div style={{height:"120px",width:"160px", border:"1px solid blue",backgroundColor:"whitesmoke",borderRadius:"10%",padding:"5px"}}>
                 <h3>surendra Dodiya</h3>
                 <h4>Profeshan:Teacher</h4>
-                <img src={car} style={{height:"70px",width:"70px"}}/>
+                <img src={car} style={{height:"60px",width:"70px"}}/>
             </div>
             </div>
 
             <div style={{display:"flex",marginTop:"10px"}}>
             <img src={santosh} height="120px" width="150px" style={{borderRadius:"10%"}}/>
-            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+            <div style={{height:"120px",width:"160px", border:"1px solid blue",backgroundColor:"whitesmoke",borderRadius:"10%",padding:"5px"}}>
                 <h3>santosh Meena</h3>
                 <h4>Profeshan:Tea bussness</h4>
                 <img src={tea} style={{height:"50px",width:"70px"}}/>
@@ -62,7 +62,7 @@ axios.post("https://election-u79u.onrender.com/stusave",input).then(()=>{
 
             <div style={{display:"flex",marginTop:"10px"}}>
             <img src={dharmendra} height="120px" width="150px" style={{borderRadius:"10%"}}/>
-            <div style={{height:"120px",width:"150px", border:"1px dashed blue"}}>
+            <div style={{height:"120px",width:"160px", border:"1px solid blue",backgroundColor:"whitesmoke",borderRadius:"10%",padding:"5px"}}>
                 <h3>Dharmendra sharma</h3>
                 <h4>Profeshan:Lic agent</h4>
                 <img src={Jhopdi} style={{height:"50px",width:"80px"}}/>
@@ -71,7 +71,7 @@ axios.post("https://election-u79u.onrender.com/stusave",input).then(()=>{
 
             <div style={{display:"flex",marginTop:"10px"}}>
             <img src={rajesh} height="120px" width="150px" style={{borderRadius:"10%"}}/>
-            <div style={{height:"120px",width:"150px", border:"1px dashed blue" }}>
+            <div style={{height:"120px",width:"160px", border:"1px solid blue",backgroundColor:"whitesmoke",borderRadius:"10%",padding:"5px"}}>
                 <h3>Rajesh Raniya</h3>
                 <h4>Profeshan:Bussness</h4>
                 <img src={peg} style={{height:"60px",width:"80px"}}/>
